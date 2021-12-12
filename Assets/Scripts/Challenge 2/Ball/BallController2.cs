189acc14c1b906cc7cd665bd8d888489a4515be1
ivanlianto59 <ivanlianto59@gmail.com>
2021-12-12 22:34:18 +0700

    Challenge 2

---
+++ Assets/Scripts/Challenge 2/Ball/BallController2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField] private float force;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        PushBall();
+    }
+
+    private void PushBall()
+    {
+        var angle = Random.Range(-45f, 45f);
+        Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+        rb.AddForce(dir * force);
+    }
+}
