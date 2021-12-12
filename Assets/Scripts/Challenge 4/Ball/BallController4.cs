684d11ffd3f1fc3c9694b3959b0078287bb9af22
ivanlianto59 <ivanlianto59@gmail.com>
2021-12-12 22:54:12 +0700

    Challenge 4

---
+++ Assets/Scripts/Challenge 4/Ball/BallController4.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController4 : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        KeyboardController();
+    }
+
+    private void KeyboardController()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector2(x, y) * speed * Time.deltaTime);
+    }
+}
