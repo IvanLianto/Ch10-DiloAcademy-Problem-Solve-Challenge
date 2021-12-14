314c303468c6d32810be060f67e510baab41fd07
ivanlianto59 <ivanlianto59@gmail.com>
2021-12-14 23:06:59 +0700

    Challenge 6

---
+++ Assets/Scripts/Challenge 6/Spawn/SpawnManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject square;
+    [SerializeField] private int numberToSpawn;
+    [SerializeField] private int counter;
+
+    private GameObject circle;
+
+    private ObjectPool OP;
+
+    void Start()
+    {
+        OP = ObjectPool.SharedInstance;
+        circle = GameObject.FindGameObjectWithTag("Player");
+        SpawnSquare();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnSquare()
+    {
+        while(counter < numberToSpawn)
+        {
+            Vector3 pos = new Vector3(Random.Range(-6.71f, 6.71f), Random.Range(-2.74f, 2.74f));
+            var spawn = OP.GetPooledObject();
+
+            if ((pos - circle.transform.position).magnitude < 3)
+            {
+                continue;
+            } else
+            {
+                spawn.transform.position = pos;
+                spawn.transform.rotation = Quaternion.identity;
+                spawn.SetActive(true);
+                counter++;
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Object Pooling/ObjectPool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool SharedInstance;
+
+    [SerializeField] private List<GameObject> pooledObjects;
+    [SerializeField] private GameObject objectToPools = null;
+    [SerializeField] private int amountToPool = 10;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+
+        pooledObjects = new List<GameObject>();
+        GameObject tmp;
+        for (var i = 0; i < amountToPool; i++)
+        {
+            tmp = Instantiate(objectToPools);
+            tmp.transform.parent = transform;
+            tmp.SetActive(false);
+            pooledObjects.Add(tmp);
+        }
+
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (var i = 0; i < amountToPool; i++)
+        {
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+}
